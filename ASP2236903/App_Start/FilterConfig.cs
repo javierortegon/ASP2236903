1ddbb16cea939a6919c61b94197e64d9dca638e1
javierortegon <javierortegonmp@gmail.com>
2021-08-04 08:57:09 -0500

    Agregar archivos de proyecto.

---
+++ ASP2236903/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ASP2236903
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP2236903/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP2236903.MvcApplication" Language="C#" %>
