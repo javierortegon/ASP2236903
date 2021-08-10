6f10317e3f8bce0bddb73e22ebbdb4e09c45842d
javierortegon <javierortegonmp@gmail.com>
2021-08-10 08:57:44 -0500

    pruebas de vistas y conroladores y inicio del crud de usuario

---
+++ ASP2236903/Controllers/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP2236903.Models;
+
+namespace ASP2236903.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities()) 
+            { 
+                return View(db.usuario.ToList());
+            }  
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.usuario.Add(usuario);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
