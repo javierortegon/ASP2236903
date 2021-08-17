e9827630eb8456fad1169408e46772dac3fba82c
javierortegon <javierortegonmp@gmail.com>
2021-08-17 08:55:12 -0500

    completando el crud de producto y ajustes del menu

--- ASP2236903/Controllers/ProductoController.cs
+++ ASP2236903/Controllers/ProductoController.cs
@@ -60,5 +60,64 @@ namespace ASP2236903.Controllers
                 return View();
             }
         }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                producto productoEdit = db.producto.Where(a => a.id == id).FirstOrDefault();
+                return View(productoEdit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(producto productoEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var oldProduct = db.producto.Find(productoEdit.id);
+                    oldProduct.nombre = productoEdit.nombre;
+                    oldProduct.cantidad = productoEdit.cantidad;
+                    oldProduct.descripcion = productoEdit.descripcion;
+                    oldProduct.percio_unitario = productoEdit.percio_unitario;
+                    oldProduct.id_proveedor = productoEdit.id_proveedor;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto producto = db.producto.Find(id);
+                    db.producto.Remove(producto);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
     }
 }
