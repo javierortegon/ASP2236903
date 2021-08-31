931b1f119fc8d64dbd02a2845385cb4af7357650
javierortegon <javierortegonmp@gmail.com>
2021-08-31 09:03:51 -0500

    creando modelo custom y utilizandolo

--- ASP2236903/Controllers/ProductoController.cs
+++ ASP2236903/Controllers/ProductoController.cs
@@ -120,5 +120,29 @@ namespace ASP2236903.Controllers
                 return View();
             }
         }
+
+        public ActionResult Reporte()
+        {
+            try
+            {
+                var db = new inventario2021Entities();
+                var query = from tabProveedor in db.proveedor
+                            join tabProducto in db.producto on tabProveedor.id equals tabProducto.id_proveedor
+                            select new Reporte
+                            {
+                                nombreProveedor = tabProveedor.nombre,
+                                telefonoProveedor = tabProveedor.telefono,
+                                direccionProveedor = tabProveedor.direccion,
+                                nombreProducto = tabProducto.nombre,
+                                PrecioProducto = tabProducto.percio_unitario
+                            };
+                return View(query);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
     }
 }
